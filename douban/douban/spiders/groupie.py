e8cd4f74e9f6793255b009f093e1b29cb49bda2d
dontry <mccoy018@gmail.com>
2015-04-14 21:11:28 +0800

    new login session

--- douban/douban/spiders/groupie.py
+++ douban/douban/spiders/groupie.py
@@ -10,11 +10,54 @@ import re
 class GroupieSpider(CrawlSpider):
     name = 'groupie'
     allowed_domains = ['douban.com']
-    start_urls = ['http://www.douban.com/group/explore?tag=%E8%B4%AD%E7%89%A9']
+    start_urls = {
+     # 'http://www.douban.com/group/explore?tag=%E8%B4%AD%E7%89%A9'
+        'http://www.douban.com/login'
+    }
 
     rules = (
         Rule(LinkExtractor(allow=r'/group/[^/]+/$'), callback='parse_group_homepage'),
     )
+
+    headers = {
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding':'gzip, deflate',
+        'Cache-Control':'max-age=0',
+        'Connection':'keep-alive',
+        'Content-Length':'77',
+        'Content-Type':'application/x-www-form-ulrencoded'
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36'
+    }
+
+    def start_requests(self):
+        print "start_requests"
+        return [Request("https//www.douban.com",meta = {'cookiejar':1}, callback = self.post_login)]
+    
+
+    def post_login(self,response):
+        print "Preparing login"
+        return [FormRequest.from_response(response,
+            meta={'cookiejar':response.meta['cookiejar']},
+            headers=self.headers,
+            formdata={
+            'form_email':'mccoy018@gmail.com',
+            'form_password':'mccoy0104130051',
+            'remember':'true'
+            },
+            callback = self.after_login
+            )]
+
+    def after_login(self,response):
+        # check login succeed before going on
+        if 'error' in response.body:
+            print 'Login Failed'
+            return 
+        else:
+            print 'Login succeed.'
+            for url in self.start_urls:
+                yield self.make_requests)from_url(url)
+            pass
+
 
     def parse_item(self, response):
         i = DoubanItem()
